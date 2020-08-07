332a10a72251d0a5d6c6bc9c5a533d787b300532
Lucas Araújo <lucas.compufc@gmail.com>
2020-08-07 00:13:27 -0300

    initial commit

---
+++ src/main/java/br/com/camelmicroserviceintegration/CamelMicroserviceIntegrationApplication.java
@@ -0,0 +1,13 @@
+package br.com.camelmicroserviceintegration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CamelMicroserviceIntegrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CamelMicroserviceIntegrationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/camelmicroserviceintegration/CamelMicroserviceIntegrationApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.camelmicroserviceintegration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CamelMicroserviceIntegrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
